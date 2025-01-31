2fe57ad0947df5d87bc4d58f217acd1360bc5c68
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-01-31 18:55:07 +0100

    feat: continuo mostra dettagli partecipante

--- src/pages/SingleTripPage.jsx
+++ src/pages/SingleTripPage.jsx
@@ -8,7 +8,7 @@ function SingleTripPage() {
     const tripId = parseInt(id);
     const [trip, setTrip] = useState({});
     const [dettaglioPartecipante, setDettaglioPartecipante] = useState();
-    const [visible, setVisible] = useState(false);
+
     const navigate = useNavigate();
 
     const getTrip = () => {
@@ -23,16 +23,19 @@ function SingleTripPage() {
 
     // funzione dove passo codice fiscale del partecipante 
     const handleClick = (partecipanti) => {
-        // setto la funzione per la visibilità a true
-        setVisible(true)
+
+
         // utilizzo la funzione find sull'array di partecipanti e ritorno quando trovo riscontro tra il codicefiscale corrente e il codice fiscale passato al click
         const curPartecipante = arrPartecipanti.find(curPart => {
             return curPart.codiceFiscale === partecipanti;
         });
         // salvo nello useState il valore che mi ritorna dalla funzione find
+
         setDettaglioPartecipante(curPartecipante)
+
+
     };
-    
+
 
     useEffect(() => {
         getTrip();
@@ -42,34 +45,47 @@ function SingleTripPage() {
 
     return (
         <>
-            {trip ? (
-                <>
-                <button className="btn btn-primary mt-4 mb-4" onClick={ () => navigate(-1)}>indietro</button>
-                <div className="card" style={{ width: "18rem" }}>
-                    <div className="card-body">
-                        <h4 className="card-title">{trip.nomeViaggio}</h4>
-                        <h6 className="card-title">{trip.destinazione}</h6>
-                        <p className="card-text">
-                            Data di partenza: {trip.dataInizio} <br />
-                            Data di ritorno: {trip.dataFine}
-                        </p>
-                        {trip.partecipanti && trip.partecipanti.map((curItem, i) => (
-                            <>
-                                <a 
-                                    className="" 
-                                    onClick={() => handleClick( curItem.codiceFiscale)} 
-                                    key={i}>
-                                        {curItem.nome}
-                                </a>
-                            </>
-                        ))}
-
-                        {/* todo sistemare questa cosa */}
-                        {/* <div className={visible ? "d-block" : "d-none"} >{dettaglioPartecipante.nome}</div> */}
+            {dettaglioPartecipante ? (
+                <section>
+                    <button className="btn btn-primary mt-4 mb-4" onClick={() => navigate(-1)}>indietro</button>
+                    <div className="card" style={{ width: "18rem" }}>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">{dettaglioPartecipante.nome}</li>
+                            <li className="list-group-item">{dettaglioPartecipante.cognome}</li>
+                            <li className="list-group-item">{dettaglioPartecipante.email}</li>
+                            <li className="list-group-item">{dettaglioPartecipante.telefono}</li>
+                            <li className="list-group-item">{dettaglioPartecipante.codiceFiscale}</li>
+                        </ul>
                     </div>
-                </div>
-           </> ) : <p>Caricamento...</p>
-            }
+
+                </section>) :
+
+                (
+                    <>
+                        <button className="btn btn-primary mt-4 mb-4" onClick={() => navigate(-1)}>indietro</button>
+                        <div className="card" style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <h4 className="card-title">{trip.nomeViaggio}</h4>
+                                <h6 className="card-title">{trip.destinazione}</h6>
+                                <p className="card-text">
+                                    Data di partenza: {trip.dataInizio} <br />
+                                    Data di ritorno: {trip.dataFine}
+                                </p>
+                                {trip.partecipanti && trip.partecipanti.map((curItem, i) => (
+                                    <>
+                                        <a
+                                            className=""
+                                            onClick={() => handleClick(curItem.codiceFiscale)}
+                                            key={i}>
+                                            {curItem.nome}
+                                        </a>
+                                    </>
+                                ))}
+
+
+                            </div>
+                        </div>
+                    </>)}
 
         </>
     )
