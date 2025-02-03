7d226bb28750795868732131570e1295491f942a
Marco Mechini <mechinimarco35@gmail.com>
2025-02-03 18:19:28 +0100

    feat: bottone per aggiunta viaggi, e commenti al codice vari

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white m-0 p-0">
-      <div className="container text-center d-flex gap-3 align-items-center justify-content-center">
+    <footer className="bg-dark text-white footer ">
+      <div className="container text-center d-flex gap-3 align-items-center justify-content-between pt-3">
         <p className="mb-4">&copy; {new Date().getFullYear()} BoolRoad - Tutti i diritti riservati.</p>
-        <ul className="list-inline d-flex align-items-center">
+        <ul className="list-inline d-flex align-items-center gap-4">
           <li className="list-inline-item">
             <a href="#" className="text-white text-decoration-none">Home</a>
           </li>
--- src/pages/TripsPage.jsx
+++ src/pages/TripsPage.jsx
@@ -39,13 +39,15 @@ function TripsPage() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setDataArr([...dataArr, formData])
-        setFormData(initialData)
+        setDataArr([...dataArr, formData]);
+        setFormData(initialData);
+        setModal(false);
     }
 
     return (
         <>
 
+            {/* modale in overlay */}
             {modal && (
                 <div className="c-modal z-2 position-fixed top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center bg-dark bg-opacity-75">
                     <div className="modal-dialog">
@@ -54,6 +56,7 @@ function TripsPage() {
                                 <h5 className="modal-title text-light">Aggiungi un viaggio</h5>
                             </div>
                             <div className="modal-body">
+                                {/* form per l'inserimento di nuovi viaggi */}
                                 <form onSubmit={handleSubmit}>
                                     <label htmlFor="nomeViaggio"></label>
                                     <input className="form-control" type="text" onChange={handleInputChange} value={formData.nomeViaggio} id="nomeViaggio" name="nomeViaggio" placeholder="nome viaggio" />
@@ -83,20 +86,32 @@ function TripsPage() {
             )}
 
             <div className="trips-page">
-                {/* {(!modal) && <button className="btn btn-primary c-modal-btn" onClick={() => setModal(true)}>+</button>} */}
-                <button className="btn btn-primary c-modal-btn" onClick={() => setModal(true)}>+</button>
-                <div className="container">
 
-                    {/* serachbar */}
-                    <div className="search-box d-flex flex-column justify-content-center align-items-center">
-                        <h3 className="search-title mb-2">Il cerca-viaggiatore!</h3>
-                        <SearchBar />
+                <div className="container">
+                    <div className="d-flex justify-content-between align-items-center">
+
+                        {/* serachbar */}
+                        <div className="search-box d-flex flex-column justify-content-center align-items-center">
+                            <h3 className="search-title mb-2">Il cerca-viaggiatore!</h3>
+                            <SearchBar />
+                        </div>
+
+                        {/* aggiungi viaggio */}
+                        <div className="add-trip text-center d-flex flex-column align-items-center gap-2 c-modal-btn">
+                            <h6>Aggiungi un <br /> viaggio!</h6>
+                            <div>
+                                {/* {(!modal) && <button className="btn btn-primary c-modal-btn" onClick={() => setModal(true)}>+</button>} */}
+                                <button className="btn btn-primary" onClick={() => setModal(true)}>+</button>
+                            </div>
+                        </div>
                     </div>
 
                     {/* lista viaggi */}
                     <div className="trips-area d-flex">
                         <ul className="d-flex flex-wrap gap-4 list-unstyled justify-content-evenly">
-                            {data.map(curViaggio => (
+
+                            {/* card viaggi */}
+                            {dataArr.map(curViaggio => (
                                 <li key={curViaggio.id} className="col-12 col-sm-12 d-flex flex-column justify-content-end">
                                     <div
                                         className="card d-flex align-items-between"
@@ -106,11 +121,15 @@ function TripsPage() {
                                         }}
                                     >
                                         <div className="overlay"></div>
+
+                                        {/* nome viaggio */}
                                         <div className="card-body d-flex justify-content-between align-items-end">
                                             <div className="card-text d-flex flex-column ">
                                                 <h5 className="card-title"><strong>{curViaggio.nomeViaggio}</strong></h5>
                                                 <h6 className="card-subtitle mb-2">{curViaggio.destinazione}</h6>
                                             </div>
+
+                                            {/* pulsante dettagli */}
                                             <div className="details-button">
                                                 <Link
                                                     to={`/trips/${curViaggio.id}`}
