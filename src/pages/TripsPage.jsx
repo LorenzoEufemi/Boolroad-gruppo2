b0ea7d553a778b982dfc6d58a12dae67e8d04d65
Marco Mechini <mechinimarco35@gmail.com>
2025-02-03 12:36:19 +0100

    feat: modifiche testo sull'immagine

--- src/pages/TripsPage.jsx
+++ src/pages/TripsPage.jsx
@@ -13,11 +13,10 @@ function TripsPage() {
             <div className="trips-area d-flex">
                 <ul className="d-flex flex-wrap gap-4 list-unstyled justify-content-evenly">
                     {data.map(curViaggio => (
-                        
                         <li key={curViaggio.id} className="col-12 col-sm-12 d-flex flex-column justify-content-end">
-                            <div className="card d-flex flex-column justify-content-end" style={{ backgroundImage: `url(${curViaggio.immagine})` }} >
-                                <div className="card-body d-flex justify-content-between">
-                                    <div className="card-text d-flex flex-column">
+                            <div className="card d-flex align-items-between" style={{ backgroundImage: `url(${curViaggio.immagine})` }} >
+                                <div className="card-body d-flex justify-content-between align-items-end">
+                                    <div className="card-text d-flex flex-column ">
                                         <h5 className="card-title">{curViaggio.nomeViaggio}</h5>
                                         <h6 className="card-subtitle mb-2 text-muted">{curViaggio.destinazione}</h6>
                                     </div>
@@ -33,7 +32,7 @@ function TripsPage() {
                                         }
                                     </div>
                                 </div>
-                            </div>                        
+                            </div>
                         </li>
                     ))}
                 </ul>
