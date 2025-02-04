0d1abe5fc749681eb32d4d2623ed19cc318d2548
TommasoVilla91 <tommasovilla@yahoo.it>
2025-02-04 09:44:02 +0100

    feat: modifiche footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-dark text-white m-0 p-0">
-      <div className="container text-center d-flex gap-3 align-items-center justify-content-center">
-        <p className="mb-4">&copy; {new Date().getFullYear()} BoolRoad - Tutti i diritti riservati.</p>
-        <ul className="list-inline d-flex align-items-center">
+      <div className="container text-center d-flex justify-content-center">        
+        <ul className="list-inline d-flex gap-3 align-items-center justify-content-center">
+          <li className="list-inline-item">
+            <p>&copy; {new Date().getFullYear()} BoolRoad - Tutti i diritti riservati.</p>
+          </li>
           <li className="list-inline-item">
             <a href="#" className="text-white text-decoration-none">Home</a>
           </li>
