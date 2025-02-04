0e759fc1ed644b8e14444153e894cbe950517a22
TommasoVilla91 <tommasovilla@yahoo.it>
2025-02-04 10:06:06 +0100

    feat: altezza footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white ">
-      <div className=" d-flex justify-content-around align-items-center" >        
+    <footer className="bg-dark text-white">
+      <div className=" d-flex justify-content-around align-items-center pt-2" >        
         <ul className="list-inline d-flex gap-3  justify-content-between">
           <li className="list-inline-item">
             <p>&copy; {new Date().getFullYear()} BoolRoad - Tutti i diritti riservati.</p>
